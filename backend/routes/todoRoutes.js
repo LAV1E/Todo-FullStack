55d3610bef190f634b403547f06c71787f540aef
Anshika Bundela <bundelaanshika1233@gmail.com>
2026-08-11 17:54:04 +0530

    add docker ci workflow

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,42 +1,3 @@
-// const express = require("express");
-// const auth = require("../middleware/authMiddleware");
-// const Todo = require("../models/Todo");
-
-// const router = express.Router();
-
-// // Protect all todo routes
-// router.use(auth);
-
-// router.get("/", async (req, res) => {
-//   const todos = await Todo.find({ user: req.user._id }).sort("-createdAt");
-//   res.json(todos);
-// });
-
-// router.post("/", async (req, res) => {
-//   const todo = await Todo.create({
-//     user: req.user._id,
-//     title: req.body.title,
-//   });
-
-//   res.status(201).json(todo);
-// });
-
-// router.patch("/:id/toggle", async (req, res) => {
-//   const todo = await Todo.findOne({ _id: req.params.id, user: req.user._id });
-
-//   todo.completed = !todo.completed;
-//   await todo.save();
-
-//   res.json(todo);
-// });
-
-// router.delete("/:id", async (req, res) => {
-//   await Todo.findOneAndDelete({ _id: req.params.id, user: req.user._id });
-//   res.json({ message: "Todo deleted" });
-// });
-
-// module.exports = router;
-
 
 const express = require("express");
 const auth = require("../middleware/authMiddleware");
@@ -110,6 +71,14 @@ router.delete("/:id", async (req, res) => {
   if (!todo) return res.status(404).json({ message: "Todo not found" });
 
   res.json({ message: "Todo deleted" });
+});
+
+router.get("/", async (req, res) => {
+  const todos = await Todo.find({ user: req.user._id }).sort("-createdAt");
+
+  console.log("Todos from MongoDB:", todos);
+
+  res.json(todos);
 });
 
 module.exports = router;
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = "https://todo-backend-v6hd.onrender.com";
+// export const API_BASE_URL = "https://todo-backend-v6hd.onrender.com";
+
+export const API_BASE_URL = "http://localhost:3000";
